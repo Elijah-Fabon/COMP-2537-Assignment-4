1abf876a78ba9ebf3685953776c8a3480c6448db
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-22 22:25:59 -0700

    fixx issue with card ids

--- index.js
+++ index.js
@@ -13,12 +13,16 @@ const setup = () => {
       secondCard.img = $(this).find(".front_face img")[0];
       secondCard.cardID = $(this).attr("id");
       if (firstCard.img.src === secondCard.img.src) {
-        $(`#(firstCard.cardID)`).parent().off("flip");
-        $(`#(secondCard.cardID)`).parent().off("flip");
+        console.log("match");
+        $(`#${firstCard.cardID}`).parent().off("flip");
+        $(`#${secondCard.cardID}`).parent().off("flip");
       } else {
+        console.log("no match");
+        console.log(firstCard.cardID);
+        console.log(secondCard.cardID);
         setTimeout(() => {
-          $(`#(firstCard.cardID)`).toggleClass("flip");
-          $(`#(secondCard.cardID)`).toggleClass("flip");
+          $(`#${firstCard.cardID}`).toggleClass("flip");
+          $(`#${secondCard.cardID}`).toggleClass("flip");
         }, 1000);
       }
       firstCard = null;
