2cc16194bd7ba8245bf867f0ccff9de5c027c411
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-22 22:02:51 -0700

    flipping cards

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const setup = () => {
+  let firstCard = null;
+  let secondCard = null;
+
   $(".card").click(function() {
     $(this).toggleClass("flip");
+    if (!firstCard) {
+      firstCard = $(this).find(".front_face")[0];
+    } else {
+      secondCard = $(this).find(".front_face")[0];
+      if (firstCard.src === secondCard.src) {
+        $(firstCard).parent().off("click");
+        $(secondCard).parent().off("click");
+      } else {
+        setTimeout(() => {
+          $(firstCard).parent().toggleClass("flip");
+          $(secondCard).parent().toggleClass("flip");
+        }, 1000);
+      }
+      firstCard = null;
+      secondCard = null;
+    }
   });
 };
 
