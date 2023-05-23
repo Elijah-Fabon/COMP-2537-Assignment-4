c2c31c90619b7022bd06628c229fe31afedf5e23
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-22 22:18:46 -0700

    fix card flip

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const setup = () => {
     $(this).toggleClass("flip");
     if (!firstCard) {
       firstCard = {};
-      firstCard.img = $(this).find(".front_face")[0];
+      firstCard.img = $(this).find(".front_face img")[0];
       firstCard.cardID = $(this).attr("id");
     } else {
       secondCard = {};
-      secondCard.img = $(this).find(".front_face")[0];
+      secondCard.img = $(this).find(".front_face img")[0];
       secondCard.cardID = $(this).attr("id");
       if (firstCard.img.src === secondCard.img.src) {
-        $(firstCard).parent().off("click");
-        $(secondCard).parent().off("click");
+        $(`#(firstCard.cardID)`).parent().off("flip");
+        $(`#(secondCard.cardID)`).parent().off("flip");
       } else {
         setTimeout(() => {
           $(`#(firstCard.cardID)`).toggleClass("flip");
